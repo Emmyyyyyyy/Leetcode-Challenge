cf354f3cbd940571e84efd3a380726e4b0cca82b
Emmyyyyyyy <eemmyy.a@gmail.com>
2023-02-11 23:57:26 +0700

    Create 20. ValidParentheses.js using Javascript

--- 20. ValidParentheses.js
+++ 20. ValidParentheses.js
@@ -2,18 +2,6 @@ var isValid = function(s) {
     if((s.length % 2) === 1)
         return false
     for(let i = 0;i<s.length;i++){
-        // if(s[i] === ')' && (s[i-1] === '[' || s[i-1] === '{' || s.includes('(', 0) === false))
-        //     return false;
-        // else if(s[i] === ']' && (s[i-1] === '(' || s[i-1] === '{' || s.includes('[', 0) === false))
-        //     return false;
-        // else if(s[i] === '}' && (s[i-1] === '[' || s[i-1] === '(' || s.includes('{', 0) === false))
-        //     return false;
-        // else if(s[i-1] === '(' && (s[i] === ']' || s[i] === '}' || s.includes(')', i) === false))
-        //     return false;
-        // else if(s[i-1] === '{' && (s[i] === ']' || s[i] === ')' || s.includes('}', i) === false))
-        //     return false;
-        // else if(s[i-1] === '[' && (s[i] === ')' || s[i] === '}' || s.includes(']', i) === false))
-        //     return false;
         if(s[i] === '(' && s[i+1] === ')'){
             s = s.slice(0, i) + s.slice(i+2, s.length)
             if(s.length === 0)
@@ -35,10 +23,8 @@ var isValid = function(s) {
             else
                 i = i-2;
         }
-        console.log(s + " " + i)
+        // console.log(s + " " + i)
     }
-    // console.log("-" + s)
-    // console.log(s.length)
     return false
 };
-console.log(isValid("(){}}{"))
+// console.log(isValid("(){}}{"))
