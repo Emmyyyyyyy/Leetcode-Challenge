3a45e6278af746fdb5a218a37305b6616caac6ba
Emmyyyyyyy <eemmyy.a@gmail.com>
2023-03-01 23:04:02 +0700

    Create 283. MoveZeroes.js using Javascript

---
+++ 283. MoveZeroes.js
@@ -0,0 +1,10 @@
+var moveZeroes = function(nums) {
+    for(let i = nums.length-1;i>=0;i--){
+        if(nums[i] === 0){
+            nums.splice(i, 1)
+            nums.push(0)
+        }
+    }
+    return nums
+};
+// console.log(moveZeroes([0,1,0,3,12]))
